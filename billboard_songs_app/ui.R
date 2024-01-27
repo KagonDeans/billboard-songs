15db9efc332e122fd6912d2dfcddb14205ac4d61
KagonDeans <kdeans0121@gmail.com>
2024-01-26 23:28:57 -0500

    presentation ready ui

--- billboard_songs_app/ui.R
+++ billboard_songs_app/ui.R
@@ -3,10 +3,10 @@ library(shiny)
 # Define UI for application that draws a histogram
 dashboardPage(skin = "green",
               dashboardHeader(title = "Billboards"),
-              dashboardSidebar(sidebarMenu(menuItem("First tab", tabName = "first_tab", icon = icon("bar-chart")),
-                                           menuItem("Second tab", tabName = "second_tab", icon = icon("")),
-                                           menuItem("Third tab", tabName = "third_tab", icon = icon("sliders")),
-                                           menuItem("Fourth tab", tabName = "fourth_tab", icon = icon("microphone-lines"))
+              dashboardSidebar(sidebarMenu(menuItem("Analyze a genre", tabName = "first_tab", icon = icon("bar-chart")),
+                                           menuItem("Make a hit song", tabName = "second_tab", icon = icon("microphone-lines")),
+                                           menuItem("Distribute that", tabName = "third_tab", icon = icon("sliders")),
+                                           menuItem("Explore an Artist", tabName = "fourth_tab", icon = icon("people-group"))
               )),
               dashboardBody(
                 tabItems(
@@ -83,13 +83,20 @@ dashboardPage(skin = "green",
                   
                   #fourth tab
                   tabItem("fourth_tab",
-                          mainPanel(
-                            textInput("artist_variable", "Enter Artist Name"),
-                            DTOutput("artistTable"),
-                            tableOutput("artist_mini_table")
-                          ),
-                          
-                          
+                          sidebarLayout(
+                            sidebarPanel(
+                      ),
+                            
+                            mainPanel(
+                              textInput("artist_variable", "Enter Artist Name"),
+                              fluidRow(DTOutput("artistTable")),
+                              fluidRow(column(width = 6, plotOutput("peakdistPlot")),
+                                       column(width = 6, plotOutput("weeksdistPlot"))),
+                              fluidRow(column(width = 12, plotOutput("peakweekboxPlot")))
+                             
+                            ),
+                            
+                          )  
                   )
                   
                   
